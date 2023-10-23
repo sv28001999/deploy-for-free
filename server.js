654108602009f23d4717f399de148ecbe50de427
Shivam <shivam033vishwakarma@gmail.com>
2023-10-24 01:38:27 +0530

    second commit

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ app.listen(process.env.PORT || 3000, () => {
 app.get('/', async (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
+
+app.get('/getInfo', async (req, res) => {
+    res.json({
+        name: 'shivam',
+        hobbie: 'programming',
+        age: 24,
+        willDevelopeProject: true
+    })
+})
